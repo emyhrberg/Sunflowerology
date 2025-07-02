160a9c49746d47dab55a829fc2bd2eeb008bd768
emyhrberg <erik.myhrberg@hotmail.com>
2025-07-02 04:46:04 +0200

    One line namespace fix

--- Common/GlobalTiles/SunflowerChanges.cs
+++ Common/GlobalTiles/SunflowerChanges.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
-namespace SienceJam.Common.GlobalTiles
+namespace Sunflowerology.Common.GlobalTiles
 {
     public class SunflowerChanges : GlobalTile
     {
