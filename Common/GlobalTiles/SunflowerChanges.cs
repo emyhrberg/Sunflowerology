2c98e00d1d48e2cda815dc5ab60604eba5d8a5cc
Cotlim <103321004+Cotlim@users.noreply.github.com>
2025-11-22 01:30:36 +0200

    Final commit?

--- Common/GlobalTiles/SunflowerChanges.cs
+++ Common/GlobalTiles/SunflowerChanges.cs
@@ -1,5 +1,6 @@
-﻿using System.Linq;
+﻿using Sunflowerology.Content.Tiles.SunflowerStagesOfGrowth;
 using Sunflowerology.Content.Tiles.SunGrass;
+using System.Linq;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
@@ -8,16 +9,18 @@ namespace Sunflowerology.Common.GlobalTiles
 {
     public class SunflowerChanges : GlobalTile
     {
+        private static int[] ogAnchors;
         public override void SetStaticDefaults()
         {
             TileObjectData sunflowerObjectData = TileObjectData.GetTileData(TileID.Sunflower, 0);
-            sunflowerObjectData.AnchorValidTiles = sunflowerObjectData.AnchorValidTiles.Append(ModContent.TileType<SunGrassTile>()).ToArray();
+            ogAnchors = sunflowerObjectData.AnchorValidTiles.ToArray();
+            sunflowerObjectData.AnchorValidTiles = sunflowerObjectData.AnchorValidTiles.Concat(NatureData.TilesToData.Keys.ToArray()).Distinct().ToArray();
         }
 
         public override void Unload()
         {
             TileObjectData sunflowerObjectData = TileObjectData.GetTileData(TileID.Sunflower, 0);
-            sunflowerObjectData.AnchorValidTiles = sunflowerObjectData.AnchorValidTiles.Except([ModContent.TileType<SunGrassTile>()]).ToArray();
+            sunflowerObjectData.AnchorValidTiles = ogAnchors;
         }
     }
 }
